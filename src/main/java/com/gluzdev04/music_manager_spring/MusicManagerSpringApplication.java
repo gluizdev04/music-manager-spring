9be618e76c4d7e1c020a6b40bf067258b30e9130
Luiz Gustavo <gluizdev@gmail.com>
2026-01-10 20:16:26 -0400

    chore: initial project setup

---
+++ src/main/java/com/gluzdev04/music_manager_spring/MusicManagerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.gluzdev04.music_manager_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicManagerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicManagerSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gluzdev04/music_manager_spring/MusicManagerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gluzdev04.music_manager_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicManagerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
