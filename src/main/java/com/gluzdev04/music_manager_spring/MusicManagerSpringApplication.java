7ac2f3ff72aeb529052b97adc0f8462e9a281d3f
Luiz Gustavo <gluizdev@gmail.com>
2026-01-13 19:13:11 -0400

    feat: implementa entidades de domínio Artista e Musica
    Cria modelagem das classes Artista e Musica com relacionamento bidirecional @OneToMany e @ManyToOne. Adiciona Enum TipoArtista para categorização.

--- src/main/java/com/gluzdev04/music_manager_spring/MusicManagerSpringApplication.java
+++ src/main/java/com/gluzdev04/music_manager_spring/MusicManagerSpringApplication.java
@@ -1,13 +1,18 @@
 package com.gluzdev04.music_manager_spring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MusicManagerSpringApplication {
+public class MusicManagerSpringApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(MusicManagerSpringApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
---
+++ src/main/java/com/gluzdev04/music_manager_spring/model/TipoArtista.java
@@ -0,0 +1,7 @@
+package com.gluzdev04.music_manager_spring.model;
+
+public enum TipoArtista {
+    solo,
+    dupla,
+    banda;
+}
